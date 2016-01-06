47a1bfeb3213ba577995e02166f4a54103673807
HongfeiXu <icevmj@gmail.com>
2016-01-06 22:49:59 +0800

    20160106

---
+++ chapter05/c5_2_shoes2.c
@@ -0,0 +1,20 @@
+/* 计算多个鞋尺码对应的英寸长度 */
+#include <stdio.h>
+#define ADJUST 7.64
+#define SCALE 0.325
+int main (void)
+{
+    double shoe, foot;
+    
+    shoe = 3.0;
+    printf ("Shoe size(men's)    foot length\n");
+    while (shoe < 18.5)
+    {
+        foot = SCALE * shoe + ADJUST;
+        printf ("%10.1f %15.2f inches\n", shoe, foot);
+        shoe = shoe + 1.0;
+    }
+    printf ("If the shoe fits, wear it.\n");
+
+    return 0;
+}
