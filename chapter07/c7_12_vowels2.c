40ecd5bf3338a3061347455c9987b5cc2db4961a
HongfeiXu <icevmj@gmail.com>
2016-01-11 21:52:26 +0800

    20160111

---
+++ chapter07/c7_12_vowels2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <ctype.h>              // 为toupper ()提供函数原型
+int main (void)
+{
+    char ch;
+    int a_ct, e_ct, i_ct, o_ct, u_ct;
+
+    a_ct = e_ct = i_ct = o_ct = u_ct = 0;
+
+    printf ("Enter some text: enter # to quit.\n");
+    while ((ch = getchar ()) != '#')
+    {
+        switch (toupper (ch))
+        {
+            case 'A':
+                a_ct++;
+                break;
+            case 'E':
+                e_ct++;
+                break;
+            case 'I':
+                i_ct++;
+                break;
+            case 'O':
+                o_ct++;
+                break;
+            case 'U':
+                u_ct++;
+                break;
+            default:
+                break;
+        }
+    }
+    printf ("number of vowels: A   E   I   O   U\n");
+    printf ("               %4d%4d%4d%4d%4d\n", 
+            a_ct, e_ct, i_ct, o_ct, u_ct);
+
+    return 0;
+}
